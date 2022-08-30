1e662dbbd1dc96155715a2473287e564df916454
OnlyDropped <asheburenkov@yandex.ru>
2022-08-31 02:05:35 +0300

    second commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$('.categories__slider').slick({
+  arrows: false,
+  centerMode: true,
+  infinite: true,
+  centerPadding: '250px',
+  slidesToShow: 1,
+  speed: 500,
+  dots: true,  
+  cssEase: 'linear',
+})
+
+
